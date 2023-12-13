5a163edf71c4f83a4813c4063864851688716fc6
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-13 14:35:28 +0100

    template update

--- template/day.py
+++ template/day.py
@@ -1,5 +1,19 @@
 from collections import defaultdict
+import os
 import re
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
-input_data = open(f"{this_folder}/input2.txt", "r").read().split("\n")
+
+
+def main(filename):
+    input_data = open(f"{this_folder}/{filename}", "r").read().split("\n")
+    # Part 1
+
+    print(f"Part 1 {filename}: ", 0)
+
+    # Part 2
+    print(f"Part 2 {filename}: ", 0)
+
+
+if __name__ == "__main__":
+    assert main("input2.txt") == (42, 24)
