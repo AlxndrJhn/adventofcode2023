803249a69dd2c7eb0b3d985990186c6f0d23fee1
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-15 07:54:49 +0100

    p2 done

--- 15/day.py
+++ 15/day.py
@@ -4,6 +4,8 @@ import re
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
+BOX_COUNT = 256
+
 
 def get_hash(string):
     current_value = 0
@@ -11,7 +13,7 @@ def get_hash(string):
         ascii_value = ord(char)
         current_value += ascii_value
         current_value *= 17
-        current_value %= 256
+        current_value %= BOX_COUNT
     return current_value
 
 
@@ -24,11 +26,34 @@ def main(filename):
     print(f"Part 1 {filename}: ", sum1)
 
     # Part 2
-    print(f"Part 2 {filename}: ", 0)
-    return sum1, 24
+    boxes = defaultdict(list)
+    for instruction in input_data:
+        label, cmd, focal_length = re.split("([=-])", instruction)
+        focal_length = int(focal_length) if focal_length else None
+        box_num = get_hash(label)
+        lense_obj = (label, focal_length)
+        idx_to_replace = next(
+            (idx for idx, obj in enumerate(boxes[box_num]) if obj[0] == label),
+            None,
+        )
+        if cmd == "=":
+            if idx_to_replace is not None:
+                boxes[box_num][idx_to_replace] = lense_obj
+            else:
+                boxes[box_num].append(lense_obj)
+        elif cmd == "-":
+            if idx_to_replace is not None:
+                boxes[box_num].pop(idx_to_replace)
+    sum2 = 0
+    for i in range(BOX_COUNT):
+        for j, (label, focal_length) in enumerate(boxes[i]):
+            sum2 += (i + 1) * (j + 1) * focal_length
+
+    print(f"Part 2 {filename}: ", sum2)
+    return sum1, sum2
 
 
 if __name__ == "__main__":
     assert get_hash("HASH") == 52
-    assert main("input2.txt") == (1320, 24)
+    assert main("input2.txt") == (1320, 145)
     main("input.txt")
