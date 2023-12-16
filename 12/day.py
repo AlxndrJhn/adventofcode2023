e9846560d855ac2be513d04a22a51a7677f6c947
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-16 20:18:00 +0100

    p2 works

--- 12/day.py
+++ 12/day.py
@@ -6,58 +6,38 @@ from functools import lru_cache
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
 Q = "?"
-OPTIONS = [".", "#"]
+D = "."
+H = "#"
 
 
-def count_arrangements(row, index, current_group_size, group_idx, groups, memo):
-    if group_idx == len(groups):
-        if current_group_size == 0:
-            return 1
-        else:
+@lru_cache(maxsize=None)
+def count_arrangements(seq, groups):
+    if len(seq) == 0:
+        return 1 if len(groups) == 0 else 0
+    if seq.startswith(D):
+        return count_arrangements(seq[1:], groups)
+    if seq.startswith(Q):
+        v1 = count_arrangements(D + seq[1:], groups)
+        v2 = count_arrangements(H + seq[1:], groups)
+        return v1 + v2
+    if seq.startswith(H):
+        if len(groups) == 0:
             return 0
-    if current_group_size > groups[group_idx]:
-        return 0
-    if index == len(row):
-        if current_group_size == groups[group_idx] and group_idx == len(groups) - 1:
-            return 1
-        else:
+        expected_length = groups[0]
+        chars = seq[:expected_length]
+        if len(chars) != expected_length:
+            return 0
+        if any(c == D for c in chars):
             return 0
 
-    if (index, current_group_size, group_idx) in memo:
-        return memo[(index, current_group_size, group_idx)]
-
-    current_char = row[index]
-    _sum = 0
-    if current_char == "?":
-        # .
-        if current_group_size:
-            if current_group_size == groups[group_idx]:
-                _sum += count_arrangements(
-                    row, index + 1, 0, group_idx + 1, groups, memo
-                )
-        else:
-            _sum += count_arrangements(row, index + 1, 0, group_idx, groups, memo)
-        # #
-        _sum += count_arrangements(
-            row, index + 1, current_group_size + 1, group_idx, groups, memo
-        )
-    elif current_char == ".":
-        if current_group_size:
-            if current_group_size == groups[group_idx]:
-                _sum += count_arrangements(
-                    row, index + 1, 0, group_idx + 1, groups, memo
-                )
-        else:
-            _sum += count_arrangements(row, index + 1, 0, group_idx, groups, memo)
-    elif current_char == "#":
-        if current_group_size < groups[group_idx]:
-            _sum += count_arrangements(
-                row, index + 1, current_group_size + 1, group_idx, groups, memo
-            )
-    else:
-        raise ValueError("Invalid character")
-    memo[(index, current_group_size, group_idx)] = _sum
-    return _sum
+        if len(seq) > expected_length:
+            if seq[expected_length] == H:
+                return 0
+            elif seq[expected_length] == Q:
+                return count_arrangements(D + seq[expected_length + 1 :], groups[1:])
+        groups = groups[1:]
+        seq = seq[expected_length:]
+        return count_arrangements(seq, groups)
 
 
 def main(filename):
@@ -72,8 +52,7 @@ def main(filename):
         seq, groups_str_before = line.split(" ")
         row, group_str = line.split()
         groups = list(map(int, group_str.split(",")))
-        memo = {}
-        arrangements = count_arrangements(row, 0, 0, 0, tuple(groups), memo)
+        arrangements = count_arrangements(row, tuple(groups))
         sum1 += arrangements
     print(f"Part 1 {filename}: ", sum1)
 
@@ -83,9 +62,8 @@ def main(filename):
         seq_before, groups_str_before = line.split(" ")
         len_of_groups_before = [int(x) for x in re.findall(r"\d+", groups_str_before)]
         seq = Q.join([seq_before] * 5)
-        len_of_groups = len_of_groups_before * 5
-        memo = {}
-        arrangements = count_arrangements(seq, 0, 0, 0, tuple(len_of_groups), memo)
+        groups = len_of_groups_before * 5
+        arrangements = count_arrangements(seq, tuple(groups))
         sum2 += arrangements
     print(f"Part 2 {filename}: ", sum2)
     return sum1, sum2
