8d256ebf831b679f3b5f48b463425e5aadead167
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-16 13:58:58 +0100

    update template

--- template/day.py
+++ template/day.py
@@ -1,12 +1,15 @@
-from collections import defaultdict
+import itertools
 import os
 import re
+from collections import defaultdict
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
 
 def main(filename):
     input_data = open(f"{this_folder}/{filename}", "r").read().split("\n")
+    input_data_mat = [list(line) for line in input_data]
+
     # Part 1
     result1 = 42
     print(f"Part 1 {filename}: ", result1)
@@ -19,3 +22,4 @@ def main(filename):
 
 if __name__ == "__main__":
     assert main("input2.txt") == (42, 24)
+    # main("input2.txt")
