62d9e40a484ac5c672e95ab034d92752635ea8d8
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-17 22:01:15 +0100

    p1 done, TIL about heapq

--- 17/day.py
+++ 17/day.py
@@ -1,10 +1,10 @@
 from copy import deepcopy
+import heapq
 import itertools
 import math
 import os
 import re
 from collections import defaultdict
-import functools
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
@@ -34,7 +34,6 @@ def main(filename):
     start_node = (0, 0)
     end_node = (W - 1, H - 1)
 
-    @functools.lru_cache(maxsize=None)
     def get_distance(node1, node2):
         if node1[2] == Dir.DOWN and node2[2] == Dir.UP:
             return float("inf")
@@ -72,35 +71,35 @@ def main(filename):
     # list of Dirs
     dirs = [Dir.UP, Dir.DOWN, Dir.LEFT, Dir.RIGHT]
 
-    # use input_data_mat[x][y] as loss function, find path from START to END using Dijkstra's algorithm
-    unvisited_nodes = set(itertools.product(range(W), range(H), dirs, range(3)))
-    shortest_path = defaultdict(lambda: float("inf"))
-    previous_nodes = {}
+    start = start_node + (Dir.RIGHT, 1)
+    unvisited_nodes = [
+        (0 if node == start else float("inf"), node)
+        for node in itertools.product(range(W), range(H), dirs, range(3))
+    ]
 
-    shortest_path[start_node[0], start_node[1], Dir.RIGHT, 1] = 0
+    heapq.heapify(unvisited_nodes)
+
+    shortest_path = defaultdict(lambda: float("inf"))
+
+    shortest_path[start] = 0
 
     while unvisited_nodes:
-        print(len(unvisited_nodes))
-        current_min_node = min(unvisited_nodes, key=lambda node: shortest_path[node])
-        if shortest_path[current_min_node] == float("inf"):
-            break
-        *node, from_dir, dir_count = current_min_node
-        node = tuple(node)
-        neighbors = get_neighbors(node)
-        for neighbor in neighbors:
-            x_n, y_n = neighbor
-            diff = (x_n - node[0], y_n - node[1])
-            dir_to_n = diff_to_dir[diff]
-            dir_count_n = dir_count + 1 if dir_to_n == from_dir else 0
-            selector = (x_n, y_n, dir_to_n, dir_count_n)
-            tentative_value = shortest_path[current_min_node] + get_distance(
-                current_min_node, selector
-            )
-            if tentative_value < shortest_path[selector]:
-                shortest_path[selector] = tentative_value
-                previous_nodes[selector] = current_min_node
+        current_shortest_distance, current_node = heapq.heappop(unvisited_nodes)
 
-        unvisited_nodes.remove(current_min_node)
+        if current_shortest_distance <= shortest_path[current_node]:
+            for neighbour in get_neighbors(current_node[:2]):
+                x_n, y_n = neighbour
+                diff = (x_n - current_node[0], y_n - current_node[1])
+                dir_to_n = diff_to_dir[diff]
+                dir_count_n = current_node[3] + 1 if dir_to_n == current_node[2] else 0
+                neighbour_node = (x_n, y_n, dir_to_n, dir_count_n)
+                distance = get_distance(current_node, neighbour_node)
+                distance_through_current_node = shortest_path[current_node] + distance
+                if distance_through_current_node < shortest_path[neighbour_node]:
+                    shortest_path[neighbour_node] = distance_through_current_node
+                    heapq.heappush(
+                        unvisited_nodes, (distance_through_current_node, neighbour_node)
+                    )
 
     all_end_nodes = [node for node in shortest_path if node[:2] == end_node]
     all_costs = [shortest_path[node] for node in all_end_nodes]
