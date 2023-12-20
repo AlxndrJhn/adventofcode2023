7c1132ca79196c518267ca05c083f41150bb99f9
Alexander Jahn <jahn.alexander@gmail.com>
2023-12-20 15:39:14 +0100

    wip

--- 20/day.py
+++ 20/day.py
@@ -49,38 +49,11 @@ def main(filename):
         if component["type"] == "conjunction":
             component["inputs"] = inputs_to_conjunction[name]
             component["input_states"] = ["low"] * len(inputs_to_conjunction[name])
+
     PUSHES = 1000
     counter = {"low": 0, "high": 0}
     for _ in range(PUSHES):
-        queue = [("broadcaster", "low", "button")]
-        while queue:
-            name, pulse, _from = queue.pop(0)
-            # print(f"{_from} -{pulse}-> {name}")
-            counter[pulse] += 1
-            component = components[name]
-            _type = component["type"]
-            if _type == "output":
-                continue
-            if _type == "broadcaster":
-                for other_name in component["connected_to"]:
-                    queue.append((other_name, pulse, name))
-            elif _type == "flip-flop":
-                if pulse == "low":
-                    component["state"] = "on" if component["state"] == "off" else "off"
-                    pulse_to_send = "low" if component["state"] == "off" else "high"
-                    for other_name in component["connected_to"]:
-                        queue.append((other_name, pulse_to_send, name))
-            elif _type == "conjunction":
-                idx_of_input = component["inputs"].index(_from)
-                component["input_states"][idx_of_input] = pulse
-                if all(state == "high" for state in component["input_states"]):
-                    pulse_to_send = "low"
-                else:
-                    pulse_to_send = "high"
-                for other_name in component["connected_to"]:
-                    queue.append((other_name, pulse_to_send, name))
-            else:
-                raise ValueError(f"Unknown component type: {name}")
+        execute_all_pulses(components, counter)
     # %flip-flop: prefix %, starts off, low pulse flips, high pulse ignored, sends low when >off, sends high when >on
     # &conjunction: prefix &, remember type of last pulse of each input, start with low pulse memory,
     #   if all(high) for all inputs, send low, high otherwise
@@ -91,12 +64,88 @@ def main(filename):
     print(f"Part 1 {filename}: ", result1)
 
     # Part 2
+    output_name = "rx"
+    if output_name not in components:
+        return result1, None
+
+    node_to_rx = next(
+        node for name, node in components.items() if output_name in node["connected_to"]
+    )
+    nodes_to_watch = node_to_rx["inputs"]
+
+    # reset components
+    counter = {"low": 0, "high": 0}
+    for name, component in components.items():
+        if component["type"] in ["broadcaster", "output"]:
+            continue
+        if component["type"] == "flip-flop":
+            component["state"] = "off"
+        elif component["type"] == "conjunction":
+            component["input_states"] = ["low"] * len(component["inputs"])
+        else:
+            raise ValueError(f"Unknown component type: {name}")
+
+    was_high_at_iter = [None] * len(nodes_to_watch)
+    iter_count = 0
+    while True:
+        execute_all_pulses(components, counter)
+        for i in range(len(nodes_to_watch)):
+            node_name = nodes_to_watch[i]
+            component = components[node_name]
+            all_memories_high = all(
+                state == "high" for state in component["input_states"]
+            )
+            if all_memories_high:
+                if was_high_at_iter[i] is None:
+                    was_high_at_iter[i] = iter_count
+                else:
+                    diff = iter_count - was_high_at_iter[i]
+                    was_high_at_iter[i] = iter_count
+                    print(f"Loop for {node_name}: {diff}")
+                # if all(was_high_at_iter):
+                #     print(f"this iteration: {iter_count}")
+                # break
+        # if all(was_high_at_iter):
+        #     break
+        iter_count += 1
     result2 = 24
     print(f"Part 2 {filename}: ", result2)
     return result1, result2
 
 
+def execute_all_pulses(components, counter):
+    queue = [("broadcaster", "low", "button")]
+    while queue:
+        name, pulse, _from = queue.pop(0)
+        # print(f"{_from} -{pulse}-> {name}")
+        counter[pulse] += 1
+        component = components[name]
+        _type = component["type"]
+        if _type == "output":
+            continue
+        if _type == "broadcaster":
+            for other_name in component["connected_to"]:
+                queue.append((other_name, pulse, name))
+        elif _type == "flip-flop":
+            if pulse == "low":
+                component["state"] = "on" if component["state"] == "off" else "off"
+                pulse_to_send = "low" if component["state"] == "off" else "high"
+                for other_name in component["connected_to"]:
+                    queue.append((other_name, pulse_to_send, name))
+        elif _type == "conjunction":
+            idx_of_input = component["inputs"].index(_from)
+            component["input_states"][idx_of_input] = pulse
+            if all(state == "high" for state in component["input_states"]):
+                pulse_to_send = "low"
+            else:
+                pulse_to_send = "high"
+            for other_name in component["connected_to"]:
+                queue.append((other_name, pulse_to_send, name))
+        else:
+            raise ValueError(f"Unknown component type: {name}")
+
+
 if __name__ == "__main__":
-    assert main("input2.txt") == (32000000, 24)
-    assert main("input3.txt") == (11687500, 24)
+    assert main("input2.txt") == (32000000, None)
+    assert main("input3.txt") == (11687500, None)
     main("input.txt")
